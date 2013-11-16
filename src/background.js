bd5eeee9d9dbcb79c6ae0912c10f64af2336ee90
zuck.ale <zuck.ale@gmail.com>
2013-11-16 13:51:11 +0100

    Add some .js

--- src/background.js
+++ src/background.js
@@ -2,142 +2,96 @@
 // ICON MANAGEMENT BASED ON iAPI PRESENCE IN SELECTED TAB
 
 var iAPIPresence = new Array();	// iAPIPresence[tab.id] tells whether the respective tab contains iAPIs ("yes") or not ("no")
-
+var disabilita = true;
 // sets the correct icon based on the presence of iAPIs in the current tab
 function setIcon(tabId) {
-	
-	if (iAPIPresence[tabId] == "yes")
- 		chrome.browserAction.setIcon({"path":"icon_green.png"});
- 	else
- 	 	chrome.browserAction.setIcon({"path":"icon_red.png"});
-
+    if (disabilita == true) {
+        chrome.browserAction.setIcon({ "path": "img/icon_black.png" });
+    }
+    else {
+        if (iAPIPresence[tabId] == "yes")
+            chrome.browserAction.setIcon({ "path": "img/icon_green.png" });
+        else
+            chrome.browserAction.setIcon({ "path": "img/icon_red.png" });
+    }
+    console.log("DISABLE: " + disabilita);
 }
 
 // listen for messages communicating iAPI presence info
-chrome.extension.onMessage.addListener(function(msg,sender,sendResponse) {
+chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
 
-	if (msg.type == "iAPI presence") {
-		iAPIPresence[sender.tab.id] = msg.presence;	// memorize presence
-		setIcon(sender.tab.id); // set icon accordingly	
-	}
- 	 	
+    if (msg.type == "iAPI presence") {
+        if (sender.tab != undefined) {
+            console.log("aggiunta tab a vettore");
+            iAPIPresence[sender.tab.id] = msg.presence;	// memorize presence
+            setIcon(sender.tab.id); // set icon accordingly	
+        }
+    }
+    else if (msg.type == "extension_status") {
+        sendResponse({ 'disabilita': disabilita });
+    }
+    
+
+
+
 });
 
-// update icon upon tab change
-chrome.tabs.onActivated.addListener( function(activeInfo) { setIcon(activeInfo.tabId); } ); 
-
-
-
-
-
-
-
-
-
-
-/*chrome.browserAction.onClicked.addListener(function(tab) {
-chrome.browserAction.setIcon({path:"icogray.gif"});
-});
-chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-
-  if (msg.color == 'green') {
-    chrome.browserAction.setIcon({path:"icogreen.gif"});
-  }
-else if(msg.color=='red'){
- chrome.browserAction.setIcon({path:"icored.gif"});
+//background open a new page (used in popup.js)
+function openTab(url) {
+    console.log("masdkamskm");
+        chrome.tabs.create({ url: url });
 }
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+
+
+    chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
+        if (disabilita == false) {
+            console.log("sendscript");
+            chrome.tabs.executeScript(tabs[0].id, {  file: 'script.js' }, function () {
+                    console.log('Successfully injected script into the page');
+                });
+        }
+        setIcon(tabs[0].id);
+    });
 });
-*/
-//Funzione per far cambiare l'icona in caso di click su di essa
-chrome.browserAction.onClicked.addListener(function(tab) {
-if(localStorage["icon"]=="icon_black.png"){
-localStorage["icon"]="icon_red.png";
+
+function changeStatusExtension() {
+    if (disabilita == true) {
+        disabilita = false;
+    } else {
+        disabilita = true;
+    }
+
 }
-else if(localStorage["icon"]=="icon_red.png"||localStorage["icon"]=="icon_green.png"){
-localStorage["icon"]="icon_black.png";
+
+function sendAllScript() {
+
+    console.log("Disable2: " + disabilita);
+    if (disabilita == false) {
+        console.log("sendscript");
+        chrome.tabs.executeScript(null, { allFrames: true, file: 'script.js' }, function () {
+            console.log('Successfully injected script into the page');
+        });
+    }
 }
-chrome.browserAction.setIcon({"path":localStorage["icon"]});
-});
-
-
-
-
-
-
-
-/*
-//Funzione che cambia il colore dell'icona se ci sono iApi nella pagina o no (riceve i messaggi dagli script)
-chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-
-  if (msg.color == 'green') {
-    localStorage["icon"]="icon_green.png";
-	chrome.browserAction.setIcon({"path":localStorage["icon"]});
-  }
-else if(msg.color=='red'){
- localStorage["icon"]="icon_red.png";
- chrome.browserAction.setIcon({"path":localStorage["icon"]});
-}
-chrome.extension.onRequest.addListener(onRequest);
-
-
-});
-*/
-
-
-
-//chrome.extension.onRequest.addListener(onRequest);
-
-
-
-//Funzione che utilizzavo per bloccare la richiesta alla pagina (inutile nel caso il drag&drop funzioni
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) { 
-	
-
-
-chrome.tabs.get(details.tabId, function (tab) { 
-source=getUrlVars(details.url)["iapisource"];
-id=getUrlVars(details.url)["iapiid"];
-localStorage["redirectUrl"]=tab.url+"?iapisource="+source+"&iapiid="+id;
-
-//source=extension.getBackgroundPage.getUrlVars(details.url)[iapisource];
-//alert(source);
-//id=getUrlVars(details.url)[iapiid];
-//redirectUrl=tab.url+"?iapisource="+source+"&iapiid="+id;
-//alert(redirectUrl);
-
-alert("Per avere questa iApi inserisci nella classe del target: \"iapi source:"+source+" iapiid:"+id+"\"");
-});
-        return {cancel:true};
-		
-    },
-    // block requests matching this url
-    {urls: ["*://*/*iapisource*iapiid*"]},
-    ["blocking"]
-);
-
-
-
-
-
-
 
 //Funzione che utilizzo per ricavarmi alcuni parametri dagli URL
 function getUrlVars(url) {
 
-                var vars = {};
-                var parts = url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-                    if(vars[key]){
-                        if(vars[key] instanceof Array){
-                            vars[key].push(value);
-                        }else{
-                            vars[key] = [vars[key], value];
-                        }
-                    }else{
-                        vars[key] = value;
-                    }
-                });
-                return vars;
-				
+    var vars = {};
+    var parts = url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        if (vars[key]) {
+            if (vars[key] instanceof Array) {
+                vars[key].push(value);
+            } else {
+                vars[key] = [vars[key], value];
             }
+        } else {
+            vars[key] = value;
+        }
+    });
+    return vars;
+
+}
 
---
+++ src/popup.js
@@ -0,0 +1,76 @@
+﻿var BG = chrome.extension.getBackgroundPage();
+
+// Run our script as soon as the document's DOM is ready.
+document.addEventListener('DOMContentLoaded', function () {
+    linksHref();
+    linksOption();
+    typeInteraction();
+    on_off();
+});
+
+//validate all <a href=".."> and the BackGroundPage open a new page
+function linksHref() {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+                BG.openTab(location);
+            };
+        })();
+    }
+}
+// get the position of option.html page
+function linksOption() {
+    $("#optionPage").click(function () {
+        chrome.tabs.create({ active: true, url: chrome.extension.getURL("html/options.html") });
+    });
+}
+//set the status of chrome extension 
+function on_off() {
+    changeColor();
+    $("#on_off").click(function () {
+        BG.changeStatusExtension();//in the BackGround set the variable disabilita (true-->false & false-->true)
+        BG.sendAllScript();//enable the script to all pages
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+            BG.setIcon(tabs[0].id);//set the relative icon of the current tab (red/green) or black if disable
+
+        });
+        changeColor();
+        window.close();//close the popup.html page
+    });
+    
+}
+// set for the id="on_off" the status of the plugin(Disabled/Enabled)
+function changeColor() {
+    console.log("disabilita: " + BG.disabilita);
+    if (BG.disabilita == true) {
+        $("#on_off").html("<b>The extension is Disabled</b>");
+        //$("#on_off").css("background-color", "red");
+    } else {
+        $("#on_off").html("<b>The extension is Enabled</b>");
+
+        //$("#on_off").css("background-color", "green");
+    }
+}
+
+//hide or show the type of Interaction (Iapi, hcard, hcalendar etc.)
+function typeInteraction() {
+    //Current tab (chrome.tabs.getSelected() is deprecated)
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        console.log("tab presence: " + BG.iAPIPresence[tabs[0].id] + " disable: " + BG.disabilita);
+            if (BG.iAPIPresence[tabs[0].id] == "yes" && BG.disabilita == false) {
+                $("#typeInteraction").html("<b>(Type) Interaction</b>");
+                $("#typeInteraction").css("color","green");
+                $('#typeInteraction').show();
+
+            } else if (BG.iAPIPresence[tabs[0].id] == "no" && BG.disabilita == false) {
+                $("#typeInteraction").html("<b>No Interaction</b>");
+                $("#typeInteraction").css("color", "red");
+                $('#typeInteraction').show();
+            }
+
+    });
+}
